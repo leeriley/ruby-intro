c895345c7e903473637e60855fab340f75c4f9e1
Lee Riley <lee@leerileydesigns.com>
2012-02-02 11:48:18 -0700

    first commit

---
+++ ruby_intro.rb
@@ -0,0 +1,29 @@
+class Ruby_Intro
+
+  def Ruby_Intro.today
+
+    Time.new
+
+  end
+  
+  def Ruby_Intro.range
+  
+    (1..1000).each {|x| puts x }
+  
+  end
+  
+  def Ruby_Intro.addition(x, y)
+  
+    x + y;
+  
+  end
+  
+  def Ruby_Intro.stringcheck(string, myword)
+  
+    string.include? myword_wrap
+    
+  end
+  
+end
+  
+    
